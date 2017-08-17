3199646f01d1a69efc5c844575578b6bb45ff011
Joe Gaffey <joe@joegaffey.com>
2017-08-17 09:25:15 +0100

    Add files via upload

---
+++ keytach/keytach.ino
@@ -0,0 +1,37 @@
+#include <Keyboard.h>
+
+const int dataIN = 10;
+const int N = 3; 
+
+unsigned long prevmicros; 
+unsigned long duration; 
+
+int setrpm;
+int rpm; 
+
+boolean currentstate; 
+boolean prevstate; 
+
+void setup()
+{
+  pinMode(dataIN,INPUT);
+  prevmicros = 0;
+  prevstate = LOW; 
+}
+
+void loop()
+{
+  currentstate = digitalRead(dataIN); 
+  if( prevstate != currentstate) 
+  {
+    if( currentstate == HIGH ) 
+    {
+      duration = (micros() - prevmicros) * N; 
+      prevmicros = micros(); 
+      rpm = (60000000/duration); 
+      Keyboard.println(rpm);
+      Keyboard.println('\n');
+    }
+  }
+  prevstate = currentstate; 
+}
